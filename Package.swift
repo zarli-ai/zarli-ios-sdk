cd454f67a675c2676913c9dc82c0ae72b23d8c7c
KA WAI Tsoi <edtsoi430@gmail.com>
2025-12-27 20:08:16 -0800

    Update to v1.3.4

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         // TODO: Replace 'url' and 'checksum' with your actual GitHub Release values.
         .binaryTarget(
             name: "ZarliSDKSwift",
-            url: "https://github.com/zarli-ai/zarli-ios-sdk/releases/download/v1.3.3/ZarliSDKSwift.xcframework.zip",
-            checksum: "a4625356fc4e2dae02eea6cd941378b003a88ef49f17c39a2f309551deebee46" 
+            url: "https://github.com/zarli-ai/zarli-ios-sdk/releases/download/v1.3.4/ZarliSDKSwift.xcframework.zip",
+            checksum: "1b7906787411b4afd7d4421b909ed5001361f5b8a40ae8f8af038f9b633088f2" 
         ),
 
         // 2. The Open-Source AdMob Adapter
