b0403a1bf664e74c2020341fe79f2a460645e897
KA WAI Tsoi <edtsoi430@gmail.com>
2026-01-11 02:11:49 -0800

    Update to 1.3.41

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
         // 1. The Binary Core SDK
         .binaryTarget(
             name: "ZarliSDKSwift",
-            url: "https://github.com/zarli-ai/zarli-ios-sdk/releases/download/1.3.40/ZarliSDKSwift.xcframework.zip",
-            checksum: "83a2ed84b2f65214636968947594eac8c154fa0fed5462778e4577145c3a8aae"
+            url: "https://github.com/zarli-ai/zarli-ios-sdk/releases/download/1.3.41/ZarliSDKSwift.xcframework.zip",
+            checksum: "b42b57a467e8a8ad50df55ce035400fef2a65f2756ff852d525deeb8f6b3cbb1"
         ),
 
         // 2. The Open-Source AdMob Adapter
